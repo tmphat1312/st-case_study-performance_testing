339be137e5c67213a36e608e90669f7f636272ca
Truong Minh Phat <tmphat1312@gmail.com>
2025-01-02 13:57:01 +0000

    Fix missing current directory annotation

--- scripts/spike_test/test_out_csv.sh
+++ scripts/spike_test/test_out_csv.sh
@@ -1,1 +1,1 @@
-mkdir -p test_results && k6 run --config ./config/spike_test.json --out csv=test_results/spike_test.csv script.js
+mkdir -p test_results && k6 run --config ./config/spike_test.json --out csv=./test_results/spike_test.csv script.js
--- scripts/stress_test/test.sh
+++ scripts/stress_test/test.sh
@@ -1,1 +1,1 @@
-mkdir -p test_results && K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=/test_results/stress_test.html k6 run --config ./config/stress_test.json --out csv=test_results/stress_test.csv script.js
+mkdir -p test_results && K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=/test_results/stress_test.html k6 run --config ./config/stress_test.json --out csv=./test_results/stress_test.csv script.js
